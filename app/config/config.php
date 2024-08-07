0c97f867b4d123fa18a86f35e6dcf6363604ac73
Filip <filipivanovic59@gmail.com>
2024-08-07 18:34:12 +0200

    setting project

--- app/config/config.php
+++ app/config/config.php
@@ -1,2 +1,11 @@
 <?php 
 
+    $servername = "localhost";
+    $db_username = "root";
+    $db_password = "";
+    $database_name = "";
+
+    $conn = mysqli_connect($servername, $db_username, $db_password, $database_name);
+    if(!$conn){
+        die("Neuspjesna konekcija!");
+    }
---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+
+   require_once "app/config/config.php"
+
+   ?>
+
+   <a href="register.php">Register</a>
