d81bc3cb078c36c89ac302eeed0b3ffcb731d7aa
Filip <filipivanovic59@gmail.com>
2024-08-08 11:53:04 +0200

    add user form started

--- app/classes/User.php
+++ app/classes/User.php
@@ -1,5 +1,23 @@
 <?php 
 
 class User{
-    
+    public function crete($name, $username, $email, $password){
+        $hashed_password = password_hash($password, PASSWORD_DEFAULT);
+
+        $sql = "INSERT INTO users (name,username,email,password) VALUES (?,?,?,?)";
+        $stmt = $this->conn->prepare($sql);
+
+        $stmt->bind_param("ssss",$name,$username,$email,$password);
+
+        $result = $stmt->execute();
+
+        if($result){
+            $_SESSION['user_id'] = $result->insert_id;
+            return true;
+        }else{
+            return false;
+        }
+
+
+    }
 }
